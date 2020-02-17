6f61bb6d55e0e35b4ff16db61e25b97eab709e07
Alex Trottier <postables@rtradetechnologies.com>
2020-02-16 18:39:20 -0800

    add streamer prototype

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	code.nfsmith.ca/nsmith/mjpeg v0.0.0-20180818020341-d476620a2598 // indirect
 	github.com/as/video v0.0.0-20190425231346-9c7205d4fb3e
+	github.com/go-ini/ini v1.52.0 // indirect
 	github.com/ipfs/go-datastore v0.3.1
 	github.com/ipfs/go-ipns v0.0.2
 	github.com/libp2p/go-libp2p v0.5.2
@@ -15,6 +16,7 @@ require (
 	github.com/libp2p/go-libp2p-record v0.1.2
 	github.com/mattn/go-mjpeg v0.0.0-20180621123927-a9cedbfd1cd8
 	github.com/minio/mc v0.0.0-20200214192222-2f2aecb09dd1
+	github.com/minio/minio-go v6.0.14+incompatible
 	github.com/minio/minio-go/v6 v6.0.48
 	github.com/multiformats/go-multiaddr v0.2.0
 	go.uber.org/zap v1.13.0
---
+++ hackathon/texiot/src/golang/streamer/streamer.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/minio/minio-go"
+)
+
+const BUFSIZE = 1024 * 8
+
+var (
+	hostAddress   = flag.String("host", "192.168.0.2:80", "the host to connect to")
+	addr          = flag.String("multi.addr", "/ip4/0.0.0.0/tcp/4006", "the multiaddr for libp2p host")
+	accessKey     = flag.String("access.key", "minio", "minio access key")
+	secretKey     = flag.String("secret.key", "minio123", "minio secret key")
+	minioEndpoint = flag.String("minio.endpoint", "0.0.0.0:9000", "minio endpoint")
+	setup         = flag.Bool("setup", true, "setup the testenv then exit")
+	mux           sync.Mutex
+	mc            *minio.Client
+)
+
+func main() {
+	minioClient, err := minio.New(*minioEndpoint, *accessKey, *secretKey, false)
+	if err != nil {
+		log.Fatal("failed to access minio endpoint ", err)
+	}
+	mc = minioClient
+	streamSrvMux := http.NewServeMux()
+	streamSrvMux.HandleFunc("/", streamHandler)
+	streamServer := http.Server{
+		Addr:    "0.0.0.0:6969",
+		Handler: streamSrvMux,
+	}
+	if err := streamServer.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func streamHandler(w http.ResponseWriter, r *http.Request) {
+	obj, err := mc.GetObject("testbucket", "videofeed", minio.GetObjectOptions{})
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer obj.Close()
+	stats, err := obj.Stat()
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	fileSize := int(stats.Size)
+
+	if len(r.Header.Get("Range")) == 0 {
+
+		contentLength := strconv.Itoa(fileSize)
+		contentEnd := strconv.Itoa(fileSize - 1)
+
+		w.Header().Set("Content-Type", "video/mp4")
+		w.Header().Set("Accept-Ranges", "bytes")
+		w.Header().Set("Content-Length", contentLength)
+		w.Header().Set("Content-Range", "bytes 0-"+contentEnd+"/"+contentLength)
+		w.WriteHeader(200)
+
+		buffer := make([]byte, BUFSIZE)
+
+		for {
+			n, err := obj.Read(buffer)
+
+			if n == 0 {
+				break
+			}
+
+			if err != nil {
+				break
+			}
+
+			data := buffer[:n]
+			w.Write(data)
+			w.(http.Flusher).Flush()
+		}
+
+	} else {
+
+		rangeParam := strings.Split(r.Header.Get("Range"), "=")[1]
+		splitParams := strings.Split(rangeParam, "-")
+
+		// response values
+
+		contentStartValue := 0
+		contentStart := strconv.Itoa(contentStartValue)
+		contentEndValue := fileSize - 1
+		contentEnd := strconv.Itoa(contentEndValue)
+		contentSize := strconv.Itoa(fileSize)
+
+		if len(splitParams) > 0 {
+			contentStartValue, err = strconv.Atoi(splitParams[0])
+
+			if err != nil {
+				contentStartValue = 0
+			}
+
+			contentStart = strconv.Itoa(contentStartValue)
+		}
+
+		if len(splitParams) > 1 {
+			contentEndValue, err = strconv.Atoi(splitParams[1])
+
+			if err != nil {
+				contentEndValue = fileSize - 1
+			}
+
+			contentEnd = strconv.Itoa(contentEndValue)
+		}
+
+		contentLength := strconv.Itoa(contentEndValue - contentStartValue + 1)
+
+		w.Header().Set("Content-Type", "video/mp4")
+		w.Header().Set("Accept-Ranges", "bytes")
+		w.Header().Set("Content-Length", contentLength)
+		w.Header().Set("Content-Range", "bytes "+contentStart+"-"+contentEnd+"/"+contentSize)
+		w.WriteHeader(206)
+
+		buffer := make([]byte, BUFSIZE)
+
+		obj.Seek(int64(contentStartValue), 0)
+
+		writeBytes := 0
+
+		for {
+			n, err := obj.Read(buffer)
+
+			writeBytes += n
+
+			if n == 0 {
+				break
+			}
+
+			if err != nil {
+				break
+			}
+
+			if writeBytes >= contentEndValue {
+				data := buffer[:BUFSIZE-writeBytes+contentEndValue+1]
+				w.Write(data)
+				w.(http.Flusher).Flush()
+				break
+			}
+
+			data := buffer[:n]
+			w.Write(data)
+			w.(http.Flusher).Flush()
+		}
+	}
+}
